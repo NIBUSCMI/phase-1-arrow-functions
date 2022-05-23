629d767fab77fa93c0f378b4266a3b4fd01f684f
Nick <nbuscem1@binghamton.edu>
2022-05-23 13:57:42 -0400

    commit tree 9c5938c9be0e6539cbf4135c41a10e2ab2c92854

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function() {
+    return 2000 / 100
+}
+console.log(divide())
+
+const square = x => x * x;
+console.log(square(2))
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(5,6))
